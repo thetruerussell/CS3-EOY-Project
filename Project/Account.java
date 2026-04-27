069509935a993fca4d0b1b20f8aba802f120c264
thetruerussell <thetruerussell@gmail.com>
2026-04-27 10:43:03 -0500

    Update Account.java

--- Project/Account.java
+++ Project/Account.java
@@ -1,12 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Account{
     private int accountNumber;
     private String accountHolderName;
+    private List<FoodItem> customFoodItem;
+    private List<Meal> customMeal;
     
     public Account(int accountNumber, String accountHolderName) {
         this.accountNumber = accountNumber;
         this.accountHolderName = accountHolderName;
+        this.customFoodItem = new ArrayList<>();
+        this.customMeal = new ArrayList<>();
     }
-    
+
+    public void setCustomFoodItem(FoodItem customFoodItem) {
+        this.customFoodItem.add(customFoodItem);
+    }
+
+    public void setCustomMeal(Meal customMeal) {
+        this.customMeal.add(customMeal);
+    }
+
     public int getAccountNumber() {
         return accountNumber;
     }
